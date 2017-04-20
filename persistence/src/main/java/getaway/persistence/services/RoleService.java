bafa1ce582f1d4b8c57c89ce33e112529e9b923c
suhrit <rimalsuhrit@gmail.com>
2017-04-20 12:25:22 -0400

    Role and UserSerivce added.

---
+++ persistence/src/main/java/getaway/persistence/services/RoleService.java
@@ -0,0 +1,6 @@
+package getaway.persistence.services;
+
+import getaway.domain.security.Role;
+
+public interface RoleService extends CRUDService<Role> {
+}
---
+++ persistence/src/main/java/getaway/persistence/services/UserService.java
@@ -0,0 +1,6 @@
+package getaway.persistence.services;
+
+import getaway.domain.User;
+
+public interface UserService extends  CRUDService<User>{
+}
