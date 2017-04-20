572910d19e39a45c5b5fe5bc48430bef4ae0e00a
suhrit <rimalsuhrit@gmail.com>
2017-04-20 13:30:02 -0400

    initial bootstrap data commit.

--- scripts/src/main/java/getaway/scripts/bootstrap/MySqlBootstrap.java
+++ scripts/src/main/java/getaway/scripts/bootstrap/MySqlBootstrap.java
@@ -1,13 +1,18 @@
 package getaway.scripts.bootstrap;
 
+import static getaway.domain.enums.UserRoleEnum.ADMIN;
+import static java.util.stream.Collectors.toList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import getaway.domain.User;
 import getaway.domain.enums.UserRoleEnum;
 import getaway.domain.security.Role;
 import getaway.persistence.services.RoleService;
 import getaway.persistence.services.UserService;
+import java.util.List;
 
 @Component
 public class MySqlBootstrap implements CommandLineRunner{
@@ -28,21 +33,35 @@ public class MySqlBootstrap implements CommandLineRunner{
 	public void run(String... strings) throws Exception {
 		loadRoles();
 		loadUsers();
-		assignUsersToDefaultRole();
 	}
 
+	@SuppressWarnings("unchecked")
 	private void loadUsers() {
+		List<Role> allRoles = (List<Role>) roleService.listAll();
 
+		User superAdminUser = new User();
+		superAdminUser.setEnabled(true);
+		superAdminUser.setUsername("suhrit+superadmin@g8away.com");
+		superAdminUser.setRoles(allRoles);
+		userService.saveOrUpdate(superAdminUser);
+
+		List<Role> adminRoles = allRoles.stream().filter(role -> role.getRole() == ADMIN).collect(toList());
+
+		User adminUser = new User();
+		adminUser.setEnabled(true);
+		adminUser.setUsername("suhrit+superadmin@g8away.com");
+		adminUser.setRoles(adminRoles);
+		userService.saveOrUpdate(adminUser);
 	}
 
+	@SuppressWarnings("unchecked")
 	private void loadRoles() {
-		Role role = new Role();
-		role.setRole(UserRoleEnum.ADMIN);
+		Role adminRole = new Role();
+		adminRole.setRole(ADMIN);
+		roleService.saveOrUpdate(adminRole);
 
-		roleService.saveOrUpdate(role);
-	}
-
-	private void assignUsersToDefaultRole() {
-
+		Role superAdminRole = new Role();
+		superAdminRole.setRole(UserRoleEnum.SUPER_ADMIN);
+		roleService.saveOrUpdate(superAdminRole);
 	}
 }
