9a0e66d390d680610780a2cec7bbd1eebcb74fd9
suhrit <rimalsuhrit@gmail.com>
2017-04-20 12:25:05 -0400

    CRUD serivce added

---
+++ persistence/src/main/java/getaway/persistence/services/CRUDService.java
@@ -0,0 +1,15 @@
+package getaway.persistence.services;
+
+import java.util.List;
+
+public interface CRUDService<T> {
+	List<?> listAll();
+
+	T getById(Integer id);
+
+	T saveOrUpdate(T domainObject);
+
+	void delete(Integer id);
+
+	void delete(T domainObject);
+}
